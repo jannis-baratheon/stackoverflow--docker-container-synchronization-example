25e398252a82b71563f7b1c7974d9f3aaf03a13f
Janusz Wisniowski <janusz.wisniowski.it@gmail.com>
2017-04-09 23:35:07 +0200

    Added a null check for DEPENDS_ON and removed a redundant background-job wait

--- client/assets/entrypoint.sh
+++ client/assets/entrypoint.sh
@@ -2,12 +2,12 @@
 
 trap 'kill $(jobs -p)' EXIT
 
-for server in $DEPENDS_ON
-do
-    /assets/wait_for_server.sh $server &
-    wait $!
-done
-
-wait $!
+if [ ! -z "$DEPENDS_ON" ]; then
+    for server in $DEPENDS_ON
+    do
+        /assets/wait_for_server.sh $server &
+        wait $!
+    done
+fi
 
 exec "$@"
